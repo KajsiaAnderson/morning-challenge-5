bcbf2b63736f2336f39180369efd5a9285572000
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-10-24 10:04:29 -0600

    find the outlier even or odd number

---
+++ findTheOutlier.js
@@ -0,0 +1,23 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a function that takes the array as an argument and returns this “outlier” N.
+
+
+function outlier(arr) {
+    
+    let evenNum = []
+    let oddNum = []
+
+    for (let i = 0; i<arr.length; i++){
+        if(arr[i] % 2 === 0){
+         evenNum.push(arr[i]) 
+        } else {
+         oddNum.push(arr[i])
+        }
+    }
+
+    return evenNum.length > oddNum.length ? oddNum : evenNum
+
+}
+
+console.log(outlier([2, 4, 0, 100, 4, 11, 2602, 36]))  //11
+console.log(outlier([160, 3, 1719, 19, 11, 13, -21]))  //160
+console.log(outlier([1, 3, 5, 7, 9, 22]))  //22
